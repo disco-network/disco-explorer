9d5b2226fa99b983a7c594b4301dc932bb8249a1
mnemonic101 <mnemonic@codemonic.net>
2016-12-03 17:47:30 +0100

    restructure project

--- scripts/core/jsonviewer.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../typings/jquery/jquery.d.ts" />
-declare module disco.core {
-    class JsonViewer {
-        static createTree(container: JQuery, json: {}): void;
-        static getHtmlTree(json: {}): string;
-        static traverseJson(container: JQuery, json: {}): void;
-    }
-}
--- source/core/common.ts
+++ source/core/common.ts
@@ -1,6 +1,6 @@
-/// <reference path="../typings/jquery/jquery.d.ts" />
-/// <reference path="../typings/jqueryui/jqueryui.d.ts" />
-/// <reference path="../typings/knockout/knockout.d.ts" />
+/// <reference path="../../typings/jquery/jquery.d.ts" />
+/// <reference path="../../typings/jqueryui/jqueryui.d.ts" />
+/// <reference path="../../typings/knockout/knockout.d.ts" />
 
 module disco.core {
 
